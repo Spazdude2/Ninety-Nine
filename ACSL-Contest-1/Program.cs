a56af6c449bf7a394838015f5e2a95fbc930d83e
Spazdude2 <spaziani96@gmail.com>
2017-12-12 12:34:17 -0500

    start of it

---
+++ ACSL-Contest-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACSL_Contest_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@"../../Sample.txt");
+            String text = file.ReadToEnd().Replace("\r\n","|");
+            String[] lines = text.Split('|').ToArray();
+            
+        }
+    }
+}
